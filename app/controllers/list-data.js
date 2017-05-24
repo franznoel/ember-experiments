7599a5d03d214e207b185305869d05d226683a69
Franz Noel Tanglao <franz@asteriainteractive.com>
2017-05-24 09:37:25 -0700

    feat: Add list-data with person list

---
+++ app/controllers/list-data.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+    people: [
+        { name: "Jack" },
+        { name: "Johanna" },
+        { name: "Aaron" },
+        { name: "Zeus" },
+        { name: "Franz" },
+        { name: "Paul" },
+        { name: "Diane" },
+    ]
+});
